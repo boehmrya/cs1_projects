15faeeb104c4a4c91e0a92a3ea5209fb5acbd272
Ryan Boehm <ryan.w.boehm@gmail.com>
2017-06-30 05:14:28 -0500

    added rotation functions

--- matrixRotations/matrixRotate.py
+++ matrixRotations/matrixRotate.py
@@ -28,21 +28,96 @@ def rotateLeft1D(matrix):
      return newMatrix
      
 
-# flips a rectangular tow-dimensional list horizontally
+# flips a rectangular two-dimensional list horizontally
 def flipH(matrix):
-     
+     newMatrix = []
+     i = 0
+     while i < len(matrix):
+          newSubMatrix = []
+          j = (len(matrix[i]) - 1)
+          while j >= 0:
+               newSubMatrix.append(matrix[i][j])
+               j -= 1
+          newMatrix.append(newSubMatrix)
+          i += 1
+     return newMatrix
 
 
+# flips a rectangular two-dimensional list vertically
+def flipV(matrix):
+     newMatrix = []
+     i = (len(matrix) - 1)
+     while i >= 0:
+          newSubMatrix = []
+          j = 0
+          while j < len(matrix[i]):
+               newSubMatrix.append(matrix[i][j])
+               j += 1
+          newMatrix.append(newSubMatrix)
+          i -= 1
+     return newMatrix
+
+
+# rotates a rectangular two-dimensional list to the right
+def rotateRight(matrix):
+     newMatrix = []
+     numCols = len(matrix[0])
+     i = 0
+     while i < numCols:
+          newSubMatrix = []
+          j = (len(matrix) - 1)
+          while j >= 0:
+               k = 0
+               while k < len(matrix[j]):
+                    if k == i:
+                         newSubMatrix.append(matrix[j][k])
+                    k += 1
+               j -= 1
+          newMatrix.append(newSubMatrix)
+          i += 1
+     return newMatrix
+
+
+# rotates a rectangular two-dimensional list to the left
+def rotateLeft(matrix):
+     newMatrix = []
+     i = (len(matrix[0]) - 1)
+     while i >= 0:
+          newSubMatrix = []
+          j = 0
+          while j < len(matrix):
+               k = 0
+               while k < len(matrix[j]):
+                    if k == i:
+                         newSubMatrix.append(matrix[j][k])
+                    k += 1
+               j += 1
+          newMatrix.append(newSubMatrix)
+          i -= 1
+     return newMatrix
+
+
+# rotates a rectangular two-dimensional list 180 degrees
+def rotate180(matrix):
+     newMatrix = []
+          
+                   
 
 def main():
      # one dimensional rotations
      matrix1 = [1,6,3,4,5]
-     print(rotateRight1D(matrix1))
-     print(rotateLeft1D(matrix1))
+     #print(rotateRight1D(matrix1))
+     #print(rotateLeft1D(matrix1))
 
      # two dimensional flips
-     matrix2 = [[1,2,3,4,],
+     matrix2 = [[1,2,3,4],
                 [5,6,7,8]]
+     matrix3 = [[1,2,3,4],
+                [5,6,7,8],
+                [9,10,11,12]]
+     #print(flipH(matrix2))
+     #print(flipV(matrix3))
+     print(rotateLeft(matrix3))
 
      
 
